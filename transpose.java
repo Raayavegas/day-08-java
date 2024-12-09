a2476e606ffea90888fc25e0ed2ef7bf79e6d308
Raayavegas <veeralarithika@gmail.com>
2024-12-09 15:11:23 +0530

    Add files via upload

---
+++ transpose.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class A 
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        int c=sc.nextInt();
+        int arr[][]=new int[r][c];
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("before transopse");
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+                System.out.print(arr[i][j]+" ");
+            }
+        }
+        System.out.println("after transpose");
+        for(int i=0;i<r;i++)
+        {
+            for(int j=0;j<c;j++)
+            {
+
+                System.out.print(arr[j][i]+" ");
+            }
+        }
+    }
+}
